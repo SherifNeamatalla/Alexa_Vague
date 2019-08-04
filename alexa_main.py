662575d4483c9bad61a017a73cba2baa0da86395
SherifNeamatalla <sherif.neamatalla@heuremo.de>
2019-08-04 14:47:59 +0200

    Finished Alexa
    Added more random responses for different scenarios for more human-like experience
    Connection to both front and backend is done.
    Added list of all atributes to get camelCase for each one.
    Merry xmas everyone

--- alexa_main.py
+++ alexa_main.py
@@ -7,20 +7,119 @@ import unidecode
 import random
 import time
 import pika
+from flask_socketio import SocketIO, send
 
 
 app = Flask(__name__)
 ask = Ask(app, "/")
+socketio = SocketIO(app)
 
 laptop_dict = dict()
-query_result = list()
 backend_url = "http://localhost:5001/api/search/alexa"
+
+attributes_list = ["processorCount","processorSpeed","displayResolutionSize","screenSize","hdd","ram","screenSize"]
+
+
+@socketio.on('connect')
+def test_connect():
+    print("Connected")
 
 #First inovcation
 @ask.launch
 def start_skill():
     welcome_message = get_random_hello_message()
     return question(welcome_message)
+
+
+
+@ask.intent("more", mapping={"value": "attribute"})
+def get_more(value):
+
+    value = get_camelCase_attribute(value)
+    #If the laptop attributes are already set from front end.
+    print(value)
+
+    if len(laptop_dict.keys()) > 0  :
+        laptop_dict.update({"intent":"more"})
+        laptop_dict.update({"intentVariable":value})
+        intent_variable = laptop_dict["intentVariable"]
+
+        query_result = get_query_result_from_backend()
+        #In case the result has any loose shit in it.
+
+        try :
+            message = get_random_result_success_message(intent_variable,query_result)
+            print("I found this laptop that I think you will like, its "+intent_variable+" is "+str(query_result[0][intent_variable]))
+            send_query_result_to_frontend(query_result)
+        except :
+            message = get_random_fail_message()
+    else : #Fail message
+        message = get_random_no_chosen_laptop_fail_message()
+
+    return statement(message)
+
+@ask.intent("less", mapping={"value": "attribute"})
+def get_less(value):
+
+    value = get_camelCase_attribute(value)
+
+    #If the laptop attributes are already set from front end.
+    if len(laptop_dict.keys()) > 0 :
+        #TODO : refine the name of attributes to match backend. IMPORTANT!!!!!!!
+        laptop_dict.update({"intent":"less"})
+        laptop_dict.update({"intentVariable":value})
+
+        intent_variable = laptop_dict["intentVariable"]
+
+        query_result = get_query_result_from_backend()
+        #In case the result has any loose shit in it.
+        print(query_result)
+        try :
+            message = get_random_result_success_message(intent_variable,query_result)
+            send_query_result_to_frontend(query_result)
+        except :
+            message = get_random_fail_message()
+    else : #Fail message
+        message = get_random_no_chosen_laptop_fail_message()
+
+    return statement(message)
+
+
+
+#Sets attributes for the laptop
+@app.route('/alexa/setter', methods=['POST'])
+def set_laptop_attributes():
+
+    print("Setting Laptop attributes ..")
+
+    data = request.get_json()
+
+    laptop_dict.clear()
+
+    laptop_dict.update(data)
+
+    return jsonify(laptop_dict)
+
+def get_query_result_from_backend():
+
+    print("Getting result from backend..")
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    req = requests.post(backend_url,data = json.dumps(laptop_dict),headers = headers )
+
+
+    response = req.json()
+
+
+    laptop_dict.clear()
+
+    return response
+
+def send_query_result_to_frontend(query_result):
+    #TODO: send query_result
+    socketio.emit('result', query_result)
+    pass
 
 def get_random_hello_message():
 
@@ -33,120 +132,57 @@ def get_random_hello_message():
 
     return random_response[0]
 
-@ask.intent("more", mapping={"value": "attribute"})
-def get_more(value):
-    #If the laptop attributes are already set from front end.
-    print(laptop_dict)
-    if len(laptop_dict.keys()) > 0  :
-        print(value)
+def get_random_no_chosen_laptop_fail_message():
 
-        #TODO : refine the name of attributes to match backend. IMPORTANT!!!!!!!
-
-        laptop_dict.update({"intent":"more"})
-        laptop_dict.update({"intentVariable":value})
-
-        query_result.clear()
-
-        query_result.append(get_query_result())
-        #In case the result has any loose shit in it.
-        try :
-            message = "I found this laptop that I think you will like, its "+laptop_dict["intentVariable"]+" is "+str(query_result[0][0][laptop_dict["intentVariable"]])
-        except :
-            message = get_random_fail_message()
-    else : #Fail message
-        message = get_random_fail_message()
-
-    return statement(message)
-
-@ask.intent("less", mapping={"value": "attribute"})
-def get_less(value):
-
-    #If the laptop attributes are already set from front end.
-    if len(laptop_dict.keys()) > 0 and "intent" not in laptop_dict.keys():
-        #TODO : refine the name of attributes to match backend. IMPORTANT!!!!!!!
-        laptop_dict.update({"intent":"less"})
-        laptop_dict.update({"intentVariable":value})
-
-        query_result.clear()
-
-        query_result.append(get_query_result())
-        #In case the result has any loose shit in it.
-        try :
-            message = "I found this laptop that I think you will like, its "+laptop_dict["intentVariable"]+" is "+str(query_result[0][0][laptop_dict["intentVariable"]])
-        except :
-            message = get_random_fail_message()
-    else : #Fail message
-        message = get_random_fail_message()
-
-    return statement(message)
-
-def get_random_fail_message():
-
-    #To feel more human like.
-    messages = ["I am sorry I couldn't find any chosen laptop"
-    ,"You seem to have forgotten to choose a laptop, please choose one first"
-    ,"You have to choose a laptop first to be able to criticize it"]
+    messages = ["You seem to have forgotten to choose a laptop, please choose one first"
+    ,"You have to choose a laptop first to be able to criticize it",
+    "I am sorry I couldn't find any chosen laptop"
+    ,"Did you choose a laptop?"
+    ,"Hey there, ready to shop for some laptops?"
+    ,"I am here, tell me what you need"
+    ,"Alexa is ready for some action, are you?"]
 
     random_response = random.sample(messages,1)
 
     return random_response[0]
 
-#Sets attributes for the laptop
-@app.route('/alexa/setter', methods=['POST'])
-def set_laptop_attributes():
+def get_random_fail_message():
 
-    data = request.get_json()
+    #To feel more human like.
+    messages = ["I seem to have encountered a problem with Laptopshop, please try again"
+    ,"There seems to be a problem, could you try again?",
+    "There was a problem with your request, please try again later"]
 
-    laptop_dict.clear()
+    random_response = random.sample(messages,1)
 
-    laptop_dict.update(data)
+    return random_response[0]
 
-    return jsonify(laptop_dict)
+def get_random_result_success_message(intent_variable,query_result):
+    message = "I found this laptop that I think you will like, its "+intent_variable+" is "+str(query_result[0][intent_variable])
 
+    #To feel more human like.
+    messages = ["I found a laptop that I think you will like"
+    ,"How about this laptop",
+    "Here is the laptop I found for you"
+    ,"Christmas is here, I just found the perfect laptop for you"]
 
-@app.route('/alexa/getQuery',methods = ['GET'])
-def return_query_to_frontend():
-    #The query has already been processed.
-    if len(query_result) > 0  and len(query_result[0]) >0:
-    #print(response)
-        json_response = jsonify(query_result[0])
-        # reset the query_result variable.
-        #query_result.clear()
+    random_response = random.sample(messages,1)
 
-        laptop_dict.clear()
-    else :
-        #Fail, result is not here yet, keep listening.
-        json_response = ""
-    print(json_response)
-    return json_response
+    brand_name = str(query_result[0]["brandName"])
+    price = str(query_result[0]["price"])
 
-def get_query_result():
+    message = str(random_response[0])+" it is "+brand_name+" which costs "+price+"euros , also its "+intent_variable+" is "+str(query_result[0][intent_variable])
 
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    print(message)
 
-    req = requests.post(backend_url,data = json.dumps(laptop_dict),headers = headers )
+    return message
 
-    response = req.json()
-
-    return response
-
-def send_rabbitMQ_message():
-    #Create a new instance of the Connection object
-
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
-    #Create a new channel with the next available channel number or pass in a channel number to use
-
-    channel = connection.channel()
-
-    #Declare queue, create if needed. This method creates or checks a queue. When creating a new queue the client can specify various properties that control the durability of the queue and its contents, and the level of sharing for the queue.
-
-    channel.queue_declare(queue='alexa')
-
-    channel.basic_publish(exchange='', routing_key='', body='OK!')
+def get_camelCase_attribute(attribute_name):
+    for attribute in attributes_list :
+        if attribute_name.lower() == attribute.lower():
+            return attribute
 
 
-    connection.close()
 
 if __name__ == "__main__":
-     app.run(port=5004,debug=True)
+    socketio.run(app,port = 5004)
